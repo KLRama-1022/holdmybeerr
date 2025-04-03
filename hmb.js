fb7964d8955b36fcdefdb67da37776e47044c4ab
KLRama-1022 <59734183+KLRama-1022@users.noreply.github.com>
2025-04-03 23:43:21 +0530

    Update hmb.js

--- hmb.js
+++ hmb.js
@@ -3,16 +3,3 @@ const darkModeToggle = document.getElementById("dark-mode-toggle");
 darkModeToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 });
-
-// Dark Mode Toggle
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const flashMessage = document.getElementById("flashMessage");
-    flashMessage.classList.add("active");
-
-    setTimeout(() => {
-        flashMessage.classList.remove("active");
-    }, 3000);
-
-    document.getElementById("contactForm").reset();
-});
